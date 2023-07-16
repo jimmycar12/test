21e8975c5254a38b49ace1c0464682ff024f3d39
jani1729 <ani1729@proton.me>
2023-07-15 23:19:31 -0700

    Add say ROCKY

--- org.jimmy.core/src/main/java/Jimmy.java
+++ org.jimmy.core/src/main/java/Jimmy.java
@@ -2,4 +2,8 @@ public class Jimmy {
     public static void sayHello() {
         System.out.println("Hi, I'm Jimmy Carter");
     }
+
+    public static void sayRocky() {
+        System.out.println("ROCKY");
+    }
 }
